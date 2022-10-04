4db242f7abf58ca961c09f0d0b5f2dff72477098
mincheol-Jeon <alsehf0714@naver.com>
2022-10-04 21:54:59 +0900

    지하철 역사 좌표 시각화

---
+++ subway.py
@@ -0,0 +1,12 @@
+'''
+서울 지하철 공간 정보를 가져와서, 역세권 종류 개념에 맞게, 근처 공간을 나누어보는 프로젝트
+초역세권,역세권,근접역세권 등등의 혼재된 개념을 시각화해서, 확인하는 간단한 시각화
+
+사용해야할 모듈
+os,glob,folium, shapefile, shapeconverter ~ mysql connecter  
+'''
+
+import os
+import pandas
+glob.glob(pathname='/Users/jeonmincheol/Personal_Project/Subway_location/subway.py')
+print('Done')
